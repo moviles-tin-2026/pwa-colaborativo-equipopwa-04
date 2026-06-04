4b0bde13f9a16bd69fabddfc05cbba9c9416e4f2
SoySamiy <carlosamir2006@gmail.com>
2026-06-04 08:19:50 -0600

    Cambio prueba

--- js/convert.js
+++ js/convert.js
@@ -1,4 +1,4 @@
-const inputField = document.getElementById('input-temp');
+lconst inputField = document.getElementById('input-temp');
 const fromUnitField = document.getElementById('input-unit');
 const toUnitField = document.getElementById('output-unit');
 const outputField = document.getElementById('output-temp');
